094027fd8ecaabdb3cd269b971b97764a6ce409c
Timotius Sitorus <timsitorus1@gmail.com>
2014-07-17 10:43:22 -0400

    stuf

--- js/game/ObjectClasses/Player.js
+++ js/game/ObjectClasses/Player.js
@@ -2,7 +2,7 @@ Player = function() {
 	Player.superclass.constructor.call(this);
 	
 	// Player settings
-	this.mGroundHeight = 65;
+	this.mGroundHeight = 50;
 	this.mHorizontalSpeed = 0;
 	this.mVerticalSpeed = 0;
 	this.mFallSpeed = 0;
